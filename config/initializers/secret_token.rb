fff6848ff49e967bd6979b014c9f238eea574684
Kuber Aaganja <eskuberaaganja@gmail.com>
2013-06-07 11:33:12 +0545

    implement some mongoid features

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '215678b146d930e3d6a18724534b7c7725b44869f669393b3af2c69de53945dcd8822cbc3e380305115978ef8ee6d4df9bcbe307b3a9de2419a38da5907cb261'
